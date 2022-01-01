50956c7fc7ec53aadad30d8cb2c9b673c723a704
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2022-01-01 21:41:20 +0100

    Added Filter section to Devices page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import Header from "components/Header/Header";
 import TopSection from "components/TopSection/TopSection";
+import FilterSection from "./FilterSection/FilterSection";
 
 interface Props {}
 
@@ -10,6 +11,7 @@ const App = (props: Props) => {
     <div className="app">
       <Header />
       <TopSection />
+      <FilterSection />
     </div>
   );
 };
---
+++ src/components/FilterSection/FilterSection.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./FilterSection.scss";
+import SearchBar from "components/SearchBar/SearchBar";
+import SortBy from "components/SortBy/SortBy";
+
+interface Props {}
+
+const FilterSection = (props: Props) => {
+  return (
+    <div className="filter-section">
+      <SortBy />
+      <SearchBar />
+    </div>
+  );
+};
+
+export default FilterSection;
