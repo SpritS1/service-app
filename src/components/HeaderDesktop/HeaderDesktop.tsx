1843effda9417712603377392a2674e73458de1b
Mateusz <mateuszpenkala@gmail.com>
2022-01-15 22:04:32 +0100

    Updated HeaderDesktop style

--- src/components/HeaderDesktop/HeaderDesktop.tsx
+++ src/components/HeaderDesktop/HeaderDesktop.tsx
@@ -10,9 +10,11 @@ const HeaderDesktop = (props: Props) => {
   return (
     <header className="header-desktop">
       <h5 className="header-desktop__title">Your devices</h5>
-      <SortBy />
-      <SearchBar />
-      <button className="header-desktop__button">ADD DEVICE</button>
+      <div className="header-desktop__container-right">
+        <SortBy />
+        <SearchBar />
+        <button className="header-desktop__button">ADD DEVICE</button>
+      </div>
     </header>
   );
 };
