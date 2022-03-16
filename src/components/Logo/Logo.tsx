a7eb0bcc1933376a8f4d747759002a06cfec9ebb
Mateusz Penkala <77857948+SpritS1@users.noreply.github.com>
2022-03-16 21:35:52 +0100

    Changed Logo styling

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "./Logo.scss";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './Logo.scss';
+import { Link } from 'react-router-dom';
 
 interface Props {}
 
 const Logo = (props: Props) => {
-  return (
-    <Link to="/" className="logo">
-      YourService
-    </Link>
-  );
+    return (
+        <Link to="/" className="logo">
+            Service <div className="logo__color">X</div>
+        </Link>
+    );
 };
 
 export default Logo;
