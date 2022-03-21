64f07b340e084a76a25e0d4362a471d2f73ac59e
Mateusz <mateuszpenkala@gmail.com>
2022-03-21 09:28:23 +0100

    Moved profile-info from ProfilePage to separate component

---
+++ src/components/ProfileInfo/ProfileInfo.tsx
@@ -0,0 +1,171 @@
+import Button from 'components/Button/Button';
+import InputBasic from 'components/InputBasic/InputBasic';
+import Popup from 'components/Popup/Popup';
+import UserImage from 'components/UserImage/UserImage';
+import { UserDataContext } from 'contexts/UserDataContext';
+import { database } from 'firebase.js';
+import { doc, DocumentData, updateDoc } from 'firebase/firestore';
+import useAuth from 'hooks/useAuth';
+import usePopup from 'hooks/usePopup';
+import React, { useContext, useEffect, useState } from 'react';
+import './ProfileInfo.scss';
+
+interface Props {
+    userData: DocumentData | null;
+}
+
+const ProfileInfo = ({ userData }: Props) => {
+    const { user } = useAuth();
+
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [email, setEmail] = useState(user!.email);
+    const [phone, setPhone] = useState('');
+    const [city, setCity] = useState('');
+    const [postCode, setPostCode] = useState('');
+    const [companyName, setCompanyName] = useState('');
+
+    const [areInputsFilled, setAreInputsFilled] = useState(false);
+
+    const {
+        popupContent,
+        setPopupContent,
+        popupType,
+        setPopupType,
+        isPopupActive,
+        setIsPopupActive,
+        resetPopup,
+    } = usePopup();
+
+    const handleProfileUpdate = async () => {
+        try {
+            resetPopup();
+            if (user && areInputsFilled) {
+                await updateDoc(doc(database, 'users_data', user.uid), {
+                    name: name,
+                    surname: surname,
+                    phone: phone,
+                    city: city,
+                    postCode: postCode,
+                    companyName: companyName,
+                });
+            }
+
+            setPopupType('default');
+            setPopupContent('Profile updated');
+            setIsPopupActive(true);
+        } catch (error) {
+            setPopupType('error');
+            setPopupContent(error as string);
+            setIsPopupActive(true);
+        }
+    };
+
+    useEffect(() => {
+        if (userData) {
+            setName(userData.name);
+            setSurname(userData.surname);
+            setPhone(userData.phone);
+            setCity(userData.city);
+            setPostCode(userData.postCode);
+            setCompanyName(userData.companyName);
+        }
+    }, [userData]);
+
+    useEffect(() => {
+        const checkIfInputsFilled = () => {
+            if (
+                name &&
+                surname &&
+                email &&
+                phone &&
+                city &&
+                postCode &&
+                companyName
+            )
+                return true;
+            else return false;
+        };
+
+        setAreInputsFilled(checkIfInputsFilled());
+    }, [name, surname, email, phone, city, postCode, companyName]);
+
+    return (
+        <div className="profile-info">
+            <div className="profile-info__top">
+                <h3 className="profile-info__title">USER DETAILS</h3>
+                <div className="profile-info__image-container">
+                    <UserImage isEditable={true} />
+                </div>
+            </div>
+
+            <InputBasic
+                placeholder="Name"
+                value={name}
+                setState={setName}
+                type={'text'}
+                required
+            />
+            <InputBasic
+                placeholder="Surname"
+                value={surname}
+                setState={setSurname}
+                type={'text'}
+                required
+            />
+            <InputBasic
+                placeholder="Email"
+                value={email as string}
+                setState={setEmail}
+                type={'email'}
+                disabled={true}
+            />
+            <InputBasic
+                placeholder="Phone"
+                value={phone}
+                setState={setPhone}
+                type={'tel'}
+                required
+            />
+            <InputBasic
+                placeholder="City"
+                value={city}
+                setState={setCity}
+                type={'text'}
+                required
+            />
+            <InputBasic
+                placeholder="Post Code"
+                value={postCode}
+                setState={setPostCode}
+                type={'text'}
+                required
+            />
+            <InputBasic
+                placeholder="Company name"
+                value={companyName}
+                setState={setCompanyName}
+                type={'text'}
+            />
+
+            <div className="profile-info__button-container">
+                <Button
+                    text={'Update profile'}
+                    backgroundColor="blue"
+                    action={handleProfileUpdate}
+                    disabled={!areInputsFilled}
+                />
+            </div>
+
+            <Popup
+                content={popupContent}
+                duration={5000}
+                type={popupType}
+                isActive={isPopupActive}
+                setIsActive={setIsPopupActive}
+            />
+        </div>
+    );
+};
+
+export default ProfileInfo;
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,96 +1,15 @@
-import Button from 'components/Button/Button';
 import ChangePassword from 'components/ChangePassword/ChangePassword';
 import HeaderDesktop from 'components/HeaderDesktop/HeaderDesktop';
-import InputBasic from 'components/InputBasic/InputBasic';
 import Loader from 'components/Loader/Loader';
-import Popup from 'components/Popup/Popup';
-import UserImage from 'components/UserImage/UserImage';
+import ProfileInfo from 'components/ProfileInfo/ProfileInfo';
 import { UserDataContext } from 'contexts/UserDataContext';
-import { database } from 'firebase.js';
-import { doc, updateDoc } from 'firebase/firestore';
-import useAuth from 'hooks/useAuth';
-import usePopup from 'hooks/usePopup';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import './ProfilePage.scss';
 
-type Props = {};
+interface Props {}
 
 const ProfilePage = (props: Props) => {
-    const { user } = useAuth();
     const { userData, isFetching } = useContext(UserDataContext);
-
-    const [name, setName] = useState('');
-    const [surname, setSurname] = useState('');
-    const [email, setEmail] = useState(user!.email);
-    const [phone, setPhone] = useState('');
-    const [city, setCity] = useState('');
-    const [postCode, setPostCode] = useState('');
-    const [companyName, setCompanyName] = useState('');
-
-    const [areInputsFilled, setAreInputsFilled] = useState(false);
-
-    const {
-        popupContent,
-        setPopupContent,
-        popupType,
-        setPopupType,
-        isPopupActive,
-        setIsPopupActive,
-        resetPopup,
-    } = usePopup();
-
-    const handleProfileUpdate = async () => {
-        try {
-            resetPopup();
-            if (user && areInputsFilled) {
-                await updateDoc(doc(database, 'users_data', user.uid), {
-                    name: name,
-                    surname: surname,
-                    phone: phone,
-                    city: city,
-                    postCode: postCode,
-                    companyName: companyName,
-                });
-            }
-
-            setPopupType('default');
-            setPopupContent('Profile updated');
-            setIsPopupActive(true);
-        } catch (error) {
-            setPopupType('error');
-            setPopupContent(error as string);
-            setIsPopupActive(true);
-        }
-    };
-
-    useEffect(() => {
-        if (userData) {
-            setName(userData.name);
-            setSurname(userData.surname);
-            setPhone(userData.phone);
-            setCity(userData.city);
-            setPostCode(userData.postCode);
-            setCompanyName(userData.companyName);
-        }
-    }, [userData]);
-
-    useEffect(() => {
-        const checkIfInputsFilled = () => {
-            if (
-                name &&
-                surname &&
-                email &&
-                phone &&
-                city &&
-                postCode &&
-                companyName
-            )
-                return true;
-            else return false;
-        };
-
-        setAreInputsFilled(checkIfInputsFilled());
-    }, [name, surname, email, phone, city, postCode, companyName]);
 
     return (
         <div className="profile-page">
@@ -98,86 +17,11 @@ const ProfilePage = (props: Props) => {
             {isFetching && <Loader />}
             {!isFetching && (
                 <div className="profile-page__main">
-                    <div className="profile-page__profile-info">
-                        <div className="profile-page__top">
-                            <h3 className="profile-page__profile-info-title">
-                                USER DETAILS
-                            </h3>
-                            <div className="profile-page__image-container">
-                                <UserImage isEditable={true} />
-                            </div>
-                        </div>
-
-                        <InputBasic
-                            placeholder="Name"
-                            value={name}
-                            setState={setName}
-                            type={'text'}
-                            required
-                        />
-                        <InputBasic
-                            placeholder="Surname"
-                            value={surname}
-                            setState={setSurname}
-                            type={'text'}
-                            required
-                        />
-                        <InputBasic
-                            placeholder="Email"
-                            value={email as string}
-                            setState={setEmail}
-                            type={'email'}
-                            disabled={true}
-                        />
-                        <InputBasic
-                            placeholder="Phone"
-                            value={phone}
-                            setState={setPhone}
-                            type={'tel'}
-                            required
-                        />
-                        <InputBasic
-                            placeholder="City"
-                            value={city}
-                            setState={setCity}
-                            type={'text'}
-                            required
-                        />
-                        <InputBasic
-                            placeholder="Post Code"
-                            value={postCode}
-                            setState={setPostCode}
-                            type={'text'}
-                            required
-                        />
-                        <InputBasic
-                            placeholder="Company name"
-                            value={companyName}
-                            setState={setCompanyName}
-                            type={'text'}
-                        />
-
-                        <div className="profile-page__button-container">
-                            <Button
-                                text={'Update profile'}
-                                backgroundColor="blue"
-                                action={handleProfileUpdate}
-                                disabled={!areInputsFilled}
-                            />
-                        </div>
-                    </div>
+                    <ProfileInfo userData={userData} />
 
                     <ChangePassword />
                 </div>
             )}
-
-            <Popup
-                content={popupContent}
-                duration={5000}
-                type={popupType}
-                isActive={isPopupActive}
-                setIsActive={setIsPopupActive}
-            />
         </div>
     );
 };
